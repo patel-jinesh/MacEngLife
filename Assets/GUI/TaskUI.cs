9143d71a3ce8787acad62da4e36011945018d575
Jinesh Patel <programmer.jinesh@gmail.com>
2019-03-25 16:26:57 -0400

    added collision with buildings

---
+++ Assets/GUI/TaskUI.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TaskUI : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnGUI() {
+        // Make a background box
+        GUI.Box(new Rect(10, 10, 100, 90), "Loader Menu");
+
+        // Make the first button. If it is pressed, Application.Loadlevel (1) will be executed
+        if (GUI.Button(new Rect(20, 40, 80, 20), "Level 1")) {
+            Debug.Log("H1");
+        }
+
+        // Make the second button.
+        if (GUI.Button(new Rect(20, 70, 80, 20), "Level 2")) {
+            Debug.Log("H2");
+        }
+    }
+}
